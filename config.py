28ef9750fdbd2a9504f92741afaba888b60f1181
dima-yurchuk <dimayurchuk20@gmail.com>
2021-05-21 16:16:11 +0300

    Deploy heroku

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ WTF_CRSF_ENAVLED = True
 # Database
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 #         'sqlite:///' + os.path.join(basedir, 'form.db')
-SQLALCHEMY_DATABASE_URI =  'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI =  'sqlite:///form.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
