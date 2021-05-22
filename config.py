f9586b7d4bd2563cd00485565f4dec8a980c5af5
dima-yurchuk <dimayurchuk20@gmail.com>
2021-05-22 18:46:36 +0300

    Deploy heroku, postgre

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SECRET_KEY = 'asfdsfsaaffdf'
+# SECRET_KEY = 'asfdsfsaaffdf'
 WTF_CRSF_ENAVLED = True
 # Database
 # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
 #         'sqlite:///' + os.path.join(basedir, 'form.db')
-SQLALCHEMY_DATABASE_URI =  'sqlite:///form.db'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# SQLALCHEMY_DATABASE_URI =  'sqlite:///form.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
