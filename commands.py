91c825dc389417c6b23778465f0d7d64290088ee
dima-yurchuk <dimayurchuk20@gmail.com>
2021-05-26 13:55:41 +0300

    Deploy heroku, postgresql

--- commands.py
+++ commands.py
@@ -14,7 +14,22 @@ def create_tables():
     db.create_all()
     click.echo("tables created!!!")
 
+
+click.command(name='create_admin')
+@with_appcontext
+def create_admin():
+    username = "test_admin"
+    email = "test_admin@gmail.com"
+    password = "11111111"
+    admin = True
+    user = User(username=username, email=email, password=password, admin=admin)
+    #print("superadmin created", u)
+    db.session.add(user)
+    db.session.commit()
+    click.echo("admin created!!!")
+
 cli.add_command(create_tables)
+cli.add_command(create_admin)
 
 
 if __name__ == '__main__':
